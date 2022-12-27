92376e378824ff26e2f99f43495f0cb49879d995
gloryan0829 <candoublej@gmail.com>
2022-12-27 13:34:54 +0900

    updated

--- testutil/localstayking/localnet/scripts/start.sh
+++ testutil/localstayking/localnet/scripts/start.sh
@@ -24,7 +24,7 @@ edit_genesis () {
 
     # Update epochs module
     dasel put string -f $GENESIS '.app_state.epochs.epochs.(.identifier=day).duration' '120s'
-    dasel put string -f $GENESIS '.app_state.epochs.epochs.(.identifier=stride_epoch).duration' '120s'
+    dasel put string -f $GENESIS '.app_state.epochs.epochs.(.identifier=stayking_epoch).duration' '120s'
 
     # Update mint module
     dasel put string -f $GENESIS '.app_state.mint.params.mint_denom' 'ustay'
