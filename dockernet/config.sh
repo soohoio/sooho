d607c4618029e61b1cf5276e64dca22d4a895613
gloryan0829 <gloryan0829@icloud.com>
2023-02-02 15:34:57 +0900

    updated

--- dockernet/config.sh
+++ dockernet/config.sh
@@ -3,6 +3,8 @@
 set -eu
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+source ${SCRIPT_DIR}/keys.sh
+
 STATE=$SCRIPT_DIR/state
 LOGS=$SCRIPT_DIR/logs
 UPGRADES=$SCRIPT_DIR/upgrades
@@ -54,15 +56,6 @@ VAL_TOKENS=5000000000000
 STAKE_TOKENS=5000000000
 ADMIN_TOKENS=1000000000
 
-# CHAIN MNEMONICS
-VAL_MNEMONIC_1="close soup mirror crew erode defy knock trigger gather eyebrow tent farm gym gloom base lemon sleep weekend rich forget diagram hurt prize fly"
-VAL_MNEMONIC_2="turkey miss hurry unable embark hospital kangaroo nuclear outside term toy fall buffalo book opinion such moral meadow wing olive camp sad metal banner"
-VAL_MNEMONIC_3="tenant neck ask season exist hill churn rice convince shock modify evidence armor track army street stay light program harvest now settle feed wheat"
-VAL_MNEMONIC_4="tail forward era width glory magnet knock shiver cup broken turkey upgrade cigar story agent lake transfer misery sustain fragile parrot also air document"
-VAL_MNEMONIC_5="crime lumber parrot enforce chimney turtle wing iron scissors jealous indicate peace empty game host protect juice submit motor cause second picture nuclear area"
-VAL_MNEMONICS=("$VAL_MNEMONIC_1","$VAL_MNEMONIC_2","$VAL_MNEMONIC_3","$VAL_MNEMONIC_4","$VAL_MNEMONIC_5")
-REV_MNEMONIC="tonight bonus finish chaos orchard plastic view nurse salad regret pause awake link bacon process core talent whale million hope luggage sauce card weasel"
-
 # STAYKING
 STAYKING_CHAIN_ID=stayking-localnet
 STAYKING_NODE_PREFIX=stayking
@@ -71,8 +64,7 @@ STAYKING_VAL_PREFIX=val
 STAYKING_DENOM=$STAYKING_DENOM
 STAYKING_RPC_PORT=26657
 STAYKING_ADMIN_ACCT=admin
-STAYKING_ADMIN_ADDRESS=sooho143umg272xger2eyurqfpjgt8u533s62mpz5weq
-STAYKING_ADMIN_MNEMONIC="stock viable sponsor length claw raccoon swift hollow inspire addict order turtle forest pony talk surface harbor bargain glide proof trigger history valid penalty"
+STAYKING_ADMIN_ADDRESS=sooho1ygs3em26qaheucpckxasxuqqej80sqt2p57nyy
 STAYKING_FEE_ADDRESS=sooho1ckh2w55t4jkdz6ck74a32mqxeyrlt8ee2ws444 #wire act strong despair apple elite glide industry journey final finish coconut repair judge payment error soul bounce public sport flee library employ position
 
 # Binaries are contigent on whether we're doing an upgrade or not
@@ -103,9 +95,6 @@ RELAYER_GAIA_EXEC="$DOCKER_COMPOSE run --rm relayer-gaia"
 RELAYER_STAYKING_ACCT=rly1
 RELAYER_GAIA_ACCT=rly2
 HOST_RELAYER_ACCTS=($RELAYER_GAIA_ACCT)
-
-#RELAYER_GAIA_MNEMONIC="fiction perfect rapid steel bundle giant blade grain eagle wing cannon fever must humble dance kitchen lazy episode museum faith off notable rate flavor"
-RELAYER_GAIA_MNEMONIC="alter old invest friend relief slot swear pioneer syrup economy vendor tray focus hedgehog artist legend antenna hair almost donkey spice protect sustain increase"
 RELAYER_MNEMONICS=("$RELAYER_GAIA_MNEMONIC")
 
 
