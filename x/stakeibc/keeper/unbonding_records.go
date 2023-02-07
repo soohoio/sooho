d4952a31e7e025c8332fcc2f5df0cf49d7e4092d
gloryan0829 <gloryan0829@icloud.com>
2023-02-07 20:44:28 +0900

    updated

--- x/stakeibc/keeper/unbonding_records.go
+++ x/stakeibc/keeper/unbonding_records.go
@@ -275,11 +275,10 @@ func (k Keeper) CleanupEpochUnbondingRecords(ctx sdk.Context, epochNumber uint64
 		for _, hostZoneUnbonding := range hostZoneUnbondings {
 			// if an EpochUnbondingRecord has any HostZoneUnbonding with non-zero balances, we don't delete the EpochUnbondingRecord
 			// because it has outstanding tokens that need to be claimed
-			if !sdk.ZeroInt().Equal(hostZoneUnbonding.NativeTokenAmount) {
+			if !hostZoneUnbonding.NativeTokenAmount.Equal(sdk.ZeroInt()) {
 				shouldDeleteEpochUnbondingRecord = false
 				break
 			}
-			k.Logger(ctx).Info(fmt.Sprintf("Test Value : %v, %v, %t, %t", hostZoneUnbonding.NativeTokenAmount, sdk.ZeroInt(), !sdk.ZeroInt().Equal(hostZoneUnbonding.NativeTokenAmount), shouldDeleteEpochUnbondingRecord))
 		}
 		if shouldDeleteEpochUnbondingRecord {
 			k.Logger(ctx).Info(fmt.Sprintf("  EpochUnbondingRecord %d - All unbondings claimed, removing record", epochUnbondingRecord.EpochNumber))
