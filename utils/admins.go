2bb379cd56aaba25314c6e79c292a6ca6e1e4020
gloryan0829 <candoublej@gmail.com>
2023-02-15 11:45:36 +0900

    build script edited

--- utils/admins.go
+++ utils/admins.go
@@ -2,6 +2,6 @@ package utils
 
 var Admins = map[string]bool{
 	"sooho1ygs3em26qaheucpckxasxuqqej80sqt2p57nyy": true,
-	"sooho10d07y265gmmuvt4z0w9aw880jnsr700jefnezl": true, // gov module
+	"sooho1k8c2m5cn322akk5wy8lpt87dd2f4yh9azg7jlh": true, // gov module
 	"sooho1cprgqxgjve4sf5sjeaugympg4hrtyr6hamfjtz": true,
 }
