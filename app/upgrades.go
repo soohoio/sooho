17ace386516f86b8bb657700ade66ef6a8d4f949
gloryan0829 <gloryan0829@icloud.com>
2023-02-24 09:51:15 +0900

    v2 upgrade code injected

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,4 +1,54 @@
 package app
 
+import (
+	"fmt"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	v2 "github.com/soohoio/stayking/app/upgrades/v2"
+	claimtypes "github.com/soohoio/stayking/x/claim/types"
+	icacallbacktypes "github.com/soohoio/stayking/x/icacallbacks/types"
+	recordtypes "github.com/soohoio/stayking/x/records/types"
+	stakeibctypes "github.com/soohoio/stayking/x/stakeibc/types"
+)
+
 func (app *StayKingApp) setupUpgradeHandlers() {
+
+	// v2 upgrade
+	app.UpgradeKeeper.SetUpgradeHandler(
+		v2.UpgradeName,
+		v2.CreateUpgradeHandler(
+			app.mm,
+			app.configurator,
+			app.appCodec,
+			app.InterchainqueryKeeper,
+			app.StakeibcKeeper,
+			app.keys[claimtypes.StoreKey],
+			app.keys[icacallbacktypes.StoreKey],
+			app.keys[recordtypes.StoreKey],
+			app.keys[stakeibctypes.StoreKey],
+		),
+	)
+
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(fmt.Errorf("Failed to read upgrade info from disk: %w", err))
+	}
+
+	if app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		return
+	}
+
+	var storeUpgrades *storetypes.StoreUpgrades
+
+	switch upgradeInfo.Name {
+	case "v2":
+		storeUpgrades = &storetypes.StoreUpgrades{
+			Deleted: []string{authz.ModuleName},
+		}
+	}
+
+	if storeUpgrades != nil {
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, storeUpgrades))
+	}
 }
--- app/upgrades/v2/upgrades.go
+++ app/upgrades/v2/upgrades.go
@@ -1,21 +1,83 @@
 package v2
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	claimtypes "github.com/soohoio/stayking/x/claim/types"
+	icacallbacktypes "github.com/soohoio/stayking/x/icacallbacks/types"
+	recordtypes "github.com/soohoio/stayking/x/records/types"
+	stakeibctypes "github.com/soohoio/stayking/x/stakeibc/types"
+
+	"fmt"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	claimmigration "github.com/soohoio/stayking/x/claim/migrations/v2"
+	icacallbacksmigration "github.com/soohoio/stayking/x/icacallbacks/migrations/v2"
+	recordsmigration "github.com/soohoio/stayking/x/records/migrations/v2"
+	stakeibcmigration "github.com/soohoio/stayking/x/stakeibc/migrations/v2"
+
+	interchainquerykeeper "github.com/soohoio/stayking/x/interchainquery/keeper"
+	stakeibckeeper "github.com/soohoio/stayking/x/stakeibc/keeper"
 )
 
 const (
 	UpgradeName = "v2"
 )
 
+// Helper function to log the migrated modules consensus versions
+func logModuleMigration(ctx sdk.Context, versionMap module.VersionMap, moduleName string) {
+	currentVersion := versionMap[moduleName]
+	ctx.Logger().Info(fmt.Sprintf("migrating module %s from version %d to version %d", moduleName, currentVersion-1, currentVersion))
+}
+
 // CreateUpgradeHandler creates an SDK upgrade handler for v2
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
+	cdc codec.Codec,
+	interchainqueryKeeper interchainquerykeeper.Keeper,
+	stakeibcKeeper stakeibckeeper.Keeper,
+	claimStoreKey storetypes.StoreKey,
+	icacallbackStorekey storetypes.StoreKey,
+	recordStoreKey storetypes.StoreKey,
+	stakeibcStoreKey storetypes.StoreKey,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		currentVersions := mm.GetVersionMap()
+		delete(vm, authz.ModuleName)
+
+		stakeibcKeeper.SetParams(ctx, stakeibctypes.DefaultParams())
+
+		// Store Migration
+
+		logModuleMigration(ctx, currentVersions, claimtypes.ModuleName)
+		if err := claimmigration.MigrateStore(ctx, claimStoreKey, cdc); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to migrate claim store")
+		}
+
+		logModuleMigration(ctx, currentVersions, icacallbacktypes.ModuleName)
+		if err := icacallbacksmigration.MigrateStore(ctx, icacallbackStorekey, cdc); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to migrate icacallbacks store")
+		}
+
+		logModuleMigration(ctx, currentVersions, recordtypes.ModuleName)
+		if err := recordsmigration.MigrateStore(ctx, recordStoreKey, cdc); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to migrate records store")
+		}
+
+		logModuleMigration(ctx, currentVersions, stakeibctypes.ModuleName)
+		if err := stakeibcmigration.MigrateStore(ctx, stakeibcStoreKey, cdc); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to migrate stakeibc store")
+		}
+
+		vm[claimtypes.ModuleName] = currentVersions[claimtypes.ModuleName]
+		vm[icacallbacktypes.ModuleName] = currentVersions[icacallbacktypes.ModuleName]
+		vm[recordtypes.ModuleName] = currentVersions[recordtypes.ModuleName]
+		vm[stakeibctypes.ModuleName] = currentVersions[stakeibctypes.ModuleName]
+
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
