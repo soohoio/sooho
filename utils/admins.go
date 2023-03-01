779f850df0338f46a47b3fcad195979be01ff697
jongseokleedev <jongsuklee45@gmail.com>
2023-03-01 23:17:50 +0900

    fix typo

--- utils/admins.go
+++ utils/admins.go
@@ -3,6 +3,6 @@ package utils
 var Admins = map[string]bool{
 	"sooho13eda6gyezyukjarmm09l0h5x5t06h5ck8yh2js": true,
 	"sooho1ygs3em26qaheucpckxasxuqqej80sqt2p57nyy": true,
-	"sooho1k8c2m5cn322akk5wy8lpt87dd2f4yh9azg7jlh": true,
+	"sooho143umg272xger2eyurqfpjgt8u533s62mpz5weq": true,
 	"sooho10d07y265gmmuvt4z0w9aw880jnsr700jefnezl": false, // gov module
 }
