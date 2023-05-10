0f210d73d28a2b5d165a031509532ce8a3f4daa6
Jacob <100544538+jongseokleedev@users.noreply.github.com>
2023-05-10 19:57:29 +0900

    fix typo

--- x/lendingpool/keeper/pool.go
+++ x/lendingpool/keeper/pool.go
@@ -52,13 +52,14 @@ func (k Keeper) CreatePool(ctx sdk.Context, msg types.MsgCreatePool) (types.MsgC
 	}
 
 	pool := types.Pool{
-		Id:             id,
-		RemainingCoins: sdk.ZeroDec(),
-		RedemptionRate: sdk.OneDec(),
-		TotalCoins:     sdk.ZeroDec(),
-		MaxDebtRatio:   msg.MaxDebtRatio,
-		InterestModel:  msg.InterestModel,
-		Denom:          msg.Denom,
+		Id:                       id,
+		RemainingCoins:           sdk.ZeroDec(),
+		RedemptionRate:           sdk.OneDec(),
+		RedemptionRateWithoutTax: sdk.OneDec(),
+		TotalCoins:               sdk.ZeroDec(),
+		MaxDebtRatio:             msg.MaxDebtRatio,
+		InterestModel:            msg.InterestModel,
+		Denom:                    msg.Denom,
 	}
 
 	id = id + 1
