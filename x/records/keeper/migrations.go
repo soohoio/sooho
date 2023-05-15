67a70bf9a259ef75fe18eedbbf2f7b0fcaae22fd
yoongbok lee <js951204@gmail.com>
2023-05-16 00:00:54 +0900

    add records migration

---
+++ x/records/keeper/migrations.go
@@ -0,0 +1,21 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v3 "github.com/soohoio/stayking/v2/x/records/migrations/v3"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	keeper Keeper
+}
+
+// NewMigrator returns a new Migrator.
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+// Migrate2to3 migrates from version 2 to 3.
+func (m Migrator) Migrate2to3(ctx sdk.Context) error {
+	return v3.MigrateStore(ctx, m.keeper.storeKey, m.keeper.Cdc)
+}
